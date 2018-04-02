9766ebcaeee23de24e8ff6d4bef31b6a0dd9e509
Kanwar Sahdra <ksingh7265@gmail.com>
2018-04-01 23:34:26 -0400

    alerts when time is up added

--- go.sh
+++ go.sh
@@ -9,24 +9,22 @@ echo_me() {
 	eval "$@" # "@", @ did NOT work - they messed up the commit message
 }
 
+# wait for the specified delay
+# if want to set timeDelay from cmdargs can just get it as the first arg, then shift and still use =$*
+timeDelay=1
+sleep $(($timeDelay*60))
+
 # store the commit message; the commit message is the first argument the script is called with
+# ok what's the commit message - specified after or before? in this case, specified in the cmd line args
 message=$*
 
-# note to self - no spaces between variable assignments
-# minutes * 60 => seconds, sleep only takes seconds
-timeDelay=1
-timeDelay=$(($timeDelay*60))
+#get a notif ... terminal-notifier is an option ... 
+echo -e "\a"
+osascript -e 'display notification with title "Commit"'  # hate that can't get the terminal icon on this...
 
-# wait for the specified delay
-sleep $timeDelay
-
-#get a notif
-say error
-
-# ok what's the commit message - specified after or before? in this case, specified in the cmd line args
-# should double check to ask if want to commit whatever's coded ? or not, if this is for max to the metal focus ... 
 
 # ***** read -p method *****
+# should double check to ask if want to commit whatever's coded ? or not, if this is for max to the metal focus ... 
 read -p "Commit? y\n 	" decide
 case $decide in
 	Y|y ) echo_me git add -A
@@ -37,20 +35,3 @@ case $decide in
 		break ;;
 	N|n ) break ;;
 esac
-
-
-# select method  - answer with a number corresponding to command
-# echo "Commit?"
-# select decide in "Y" "N"
-# do
-# 	case $decide in
-# 		"Y") 
-# 			echo_me git add -A
-# 			echo_me git commit -m \""$message"\" # "$*" worked too, but this feels safer ... maybe nonsense
-# 			# also note that if want to add other options to this - ex. changing the amount of time, via command line argument, $* will be problematic
-# 				# ... could parse the string, or figure out how to have char[space]char be considered $1, the first argument even though space within , and then have a 2nd arg, $2 after that
-# 			echo_me git push origin master 
-# 			break;;
-# 		"N") break;;
-# 	esac
-# done
